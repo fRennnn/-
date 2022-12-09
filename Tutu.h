5bbf51810274c92be651d620910bcc5c74975dab
fRen <102521678+fRennnn@users.noreply.github.com>
2022-12-09 16:39:02 +0800

    Update Tutu.h

--- Tutu.h
+++ Tutu.h
@@ -5,6 +5,23 @@
 #include<windows.h>
 using namespace std;
 
+#define UserNumber 100
+
 bool IsVip = false;
 void Menu();
 void gotoxy(int x,int y);
+
+class User
+{
+    public:
+    User();
+    ~User();
+    void Registers();
+    void Login();
+    void Save();
+    void Read();
+    private:
+    string password;
+}Us;
+
+User user[UserNumber];//
