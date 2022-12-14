669d3ad356c407c0f7f70043cb23c9f7816532ef
fRen <102521678+fRennnn@users.noreply.github.com>
2022-12-14 20:09:51 +0800

    Update Tutu.h

--- Tutu.h
+++ Tutu.h
@@ -11,15 +11,17 @@ using namespace std;
 int scount = 0;
 bool IsVip = false;//判断是不是VIP
 bool ZHUANGTAI = false;//判断文件是否成功打开
+bool UpOrDown = false;//判断用户有无上号
+int Location = 0;//用户账号所在的位置
 void Menu();
 void gotoxy(int x,int y);
 
 class User
 {
     private:
-    string phone;
     string password;
     public:
+    string phone;
     User(){};
     void Registers();
     void Login();
