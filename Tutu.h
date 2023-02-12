ac69059056a59b695d8e611fd5d4175e35763468
fRen <102521678+fRennnn@users.noreply.github.com>
2023-02-12 19:15:42 +0800

    Update Tutu.h

--- Tutu.h
+++ Tutu.h
@@ -9,7 +9,9 @@
 #include<queue>
 using namespace std;
 
-#define SIZE 10
+#define SIZE 10 //最大用户数
+#define maxsize 50//最大景点数 
+#define INFINITY 32767     //INFINITY表示极大值、无穷大
 int scount = 0;
 bool IsVip = false;//判断是不是VIP
 bool ZHUANGTAI = false;//判断文件是否成功打开
@@ -44,11 +46,21 @@ class MapNode//顶点信息
     string Imformation;//地图信息，描述这个建筑
     int Number;//顶点编号
 };
-MapNode Point[15];//15个地图节点
-
+MapNode Point[maxsize];//50个地图节点
+class MGraph //邻接矩阵 
+{
+public:
+    MGraph();     //构造函数
+    ~MGraph( ){ };            //析构函数
+private:
+ MapNode vertex[maxsize];           //存放图中顶点的数组
+    int edge[maxsize][maxsize];           //存放图中边的数组
+    int vertexNum, edgeNum;              //图的顶点数和边数
+ };
+void f2(string str, string &str1, string &str2, string &str3);
+int f1(string str);
+int inputString(char *filename,string str[]);
 void CountMenu();
 void PrintMap();
 void MapCheck(int);// ^ ^
 void MapRead();
-
-
