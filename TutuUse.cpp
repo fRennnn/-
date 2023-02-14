bf0477643348974252138c07ae4b3ca9524c4053
fRen <102521678+fRennnn@users.noreply.github.com>
2023-02-15 01:23:00 +0800

    Update TutuUse.cpp

--- TutuUse.cpp
+++ TutuUse.cpp
@@ -1,14 +1,15 @@
 #include"TutuSm.h"
 int main()
 {
-    MapRead();//把txt文件数据都读取进去
+    //把txt文件数据都读取进去
     while(1)
     {
-
-        if(MapPrintOk==true)PrintMap();//打开地图后保留地图，输入选项再关闭
         Menu();//累死爷了
     }    
-  
+    
+   /*int x=asdasd();
+   cout<<x<<TEST;
+   */
     system("pause");
     return 0;
 }
